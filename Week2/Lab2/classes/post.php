5a63ef802c7374c39ac29b0faaa9222cad5e5357
Jake Attard <jakeattard18@gmail.com>
2019-07-16 21:23:00 +1000

    Week 1 Complete / Week 2 Added

--- Week2/Lab2/classes/post.php
+++ Week2/Lab2/classes/post.php
@@ -4,10 +4,16 @@ namespace wap;
 class Post {
     protected $user;
     protected $message;
+    protected $image;
+    protected $date;
+    protected $comments;
 
-    function __construct($user, $message) {
+    function __construct($user, $message, $image, $date) {
         $this->user = $user;
         $this->message = $message;
+        $this->image = $image;
+        $this->date = $date;
+        $this->comments = [];
     }
 
     function getUser() {
@@ -16,6 +22,22 @@ class Post {
 
     function getMessage() {
         return $this->message;
+    }
+
+    function getImage() {
+        return $this->image;
+    }
+
+    function getDate() {
+        return $this->date;
+    }
+
+    function getComment() {
+        return $this->comments;
+    }
+
+    function addComment($user, $comment) {
+        $this->comments[] = array("user" => $user, "comment" => $comment); 
     }
 }
 ?>
--- Week2/Lab2/classes/postSeeder.php
+++ Week2/Lab2/classes/postSeeder.php
@@ -7,9 +7,9 @@ include 'post.php';
 class PostSeeder {
     public static function seed() {
         $posts = [];
-        $posts[] = new Post("Jake", "I just joined! Add me");
-        $posts[] = new Post("Luke", "I just joined! How do i use this?");
-        $posts[] = new Post("Mary", "I just joined! Add me for free money");
+        $posts[] = new Post("Jake", "I just joined! Add me", "images/p1.jpg", "15/07/2019");
+        $posts[] = new Post("Luke", "I just joined! How do i use this?", "images/p3.png", "16/07/2019");
+        $posts[] = new Post("Mary", "I just joined! Add me for free money", "images/p2.png", "17/07/2019");
         return $posts;
     }
 }
